1319e912580e6882e951a13884169fb8a8891750
prugio-yt <111791650+prugio-yt@users.noreply.github.com>
2022-12-12 00:16:23 +0900

    feat(main,news): Add popup and news for vote

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,11 +1,20 @@
 const popupBtn = document.getElementById('popup-btn');
 const closeBtn = document.getElementById('close-popup');
+const closeBtn2 = document.getElementById('close-popup2');
 const popupWrapper = document.getElementById('popup-wrapper');
+const popupWrapper2 = document.getElementById('popup-wrapper2');
 const popupBody = document.getElementById('popup-body');
 
 closeBtn.addEventListener("click", () => {
     popupWrapper.classList.add('popup-wrapper-hide');
     popupWrapper.classList.remove('popup-wrapper-show');
+    popupBody.scrollTop = 0;
+    document.body.classList.remove('scroll-stop');
+})
+
+closeBtn2.addEventListener("click", () => {
+    popupWrapper2.classList.add('popup-wrapper-hide');
+    popupWrapper2.classList.remove('popup-wrapper-show');
     popupBody.scrollTop = 0;
     document.body.classList.remove('scroll-stop');
 })
